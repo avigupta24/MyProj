b1adbd3198eee6b9674291c4c439cdedac40a6e3
Avinash Gupta <guptaavilnct@gmail.com>
2020-06-01 12:26:19 +0530

    app update

--- app.py
+++ app.py
@@ -4,24 +4,22 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
-var1=[
-    {
-    'name':'Avinash',
-    'job' : 'DBA'
-    },
-    {
-        'name':'Priya',
-        'job':'Housewife'
-    }
-]
+# var1=[
+#     {
+#     'name':'Avinash',
+#     'job' : 'DBA'
+#     },
+#     {
+#         'name':'Priya',
+#         'job':'Housewife'
+#     }
+# ]
 
 @app.route("/")
 def home():
-    return render_template('home.html',posts=var1)
+    return ("Welcome")
+    #return render_template('home.html',posts=var1)
 
-@app.route("/about/")
-def about():
-    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True)
